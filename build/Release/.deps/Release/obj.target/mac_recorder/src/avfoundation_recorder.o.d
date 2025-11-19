dfa59cddd83c00d89d69e6c97eabff14171a187f
onurhan1337 <onurhandtr@gmail.com>
2025-11-19 19:38:31 +0300

    remove: delete generated build files and configuration for mac_recorder

--- build/Release/.deps/Release/obj.target/mac_recorder/src/avfoundation_recorder.o.d
+++
@@ -1,7 +0,0 @@
-cmd_Release/obj.target/mac_recorder/src/avfoundation_recorder.o := c++ '-DNODE_GYP_MODULE_NAME=mac_recorder' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_GLIBCXX_USE_CXX11_ABI=1' '-D_FILE_OFFSET_BITS=64' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DNAPI_DISABLE_CPP_EXCEPTIONS' '-DBUILDING_NODE_EXTENSION' -I/Users/onur/Library/Caches/node-gyp/24.5.0/include/node -I/Users/onur/Library/Caches/node-gyp/24.5.0/src -I/Users/onur/Library/Caches/node-gyp/24.5.0/deps/openssl/config -I/Users/onur/Library/Caches/node-gyp/24.5.0/deps/openssl/openssl/include -I/Users/onur/Library/Caches/node-gyp/24.5.0/deps/uv/include -I/Users/onur/Library/Caches/node-gyp/24.5.0/deps/zlib -I/Users/onur/Library/Caches/node-gyp/24.5.0/deps/v8/include -I/Users/onur/codes/node-mac-recorder/node_modules/node-addon-api  -O3 -gdwarf-2 -fno-strict-aliasing -mmacosx-version-min=10.15 -arch arm64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++20 -stdlib=libc++ -fno-rtti -ObjC++  -MMD -MF ./Release/.deps/Release/obj.target/mac_recorder/src/avfoundation_recorder.o.d.raw -c -o Release/obj.target/mac_recorder/src/avfoundation_recorder.o ../src/avfoundation_recorder.mm
-Release/obj.target/mac_recorder/src/avfoundation_recorder.o: \
-  ../src/avfoundation_recorder.mm ../src/logging.h \
-  ../src/sync_timeline.h
-../src/avfoundation_recorder.mm:
-../src/logging.h:
-../src/sync_timeline.h:
